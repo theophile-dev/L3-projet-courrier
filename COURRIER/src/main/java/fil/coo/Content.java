a416bb4d2f2896defbbc66484dfb39057df9efb7
cosse <theophile.cosse.etu@univ-lille.fr>
2019-10-15 11:41:38 +0200

    feat: add Letter

---
+++ COURRIER/src/main/java/fil/coo/Content.java
@@ -0,0 +1,5 @@
+package fil.coo;
+
+public interface Content {
+
+}
---
+++ COURRIER/src/main/java/fil/coo/LetterTest.java
@@ -0,0 +1,14 @@
+package fil.coo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LetterTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
